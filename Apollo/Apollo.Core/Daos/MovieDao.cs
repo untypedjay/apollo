8a93a7ab462402e72d41f34f7e46ad3888ce6500
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 22:30:40 +0100

    Add MovieDao boilerplate

---
+++ Apollo/Apollo.Core/Daos/MovieDao.cs
@@ -0,0 +1,50 @@
+﻿using Apollo.Core.Interface.Daos;
+using Apollo.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Daos
+{
+    public abstract class MovieDao : IMovieDao
+    {
+        public Task<bool> DeleteAsync(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> FindAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> FindByGenreAsync(string genre)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> FindByLengthGreaterAsync(double length)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> FindByLengthLessAsync(double length)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> FindByTitleAsync(string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
