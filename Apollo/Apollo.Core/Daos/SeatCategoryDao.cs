4d6b1eb8fd5aae4cca53bde4f5dbd68a9ac67d3b
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 22:34:16 +0100

    Add SeatCategoryDao boilerplate

---
+++ Apollo/Apollo.Core/Daos/SeatCategoryDao.cs
@@ -0,0 +1,47 @@
+﻿using Apollo.Core.Interface.Daos;
+using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Daos
+{
+    public abstract class SeatCategoryDao : ISeatCategoryDao
+    {
+        public Task<bool> DeleteAsync(SeatCategory seatCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SeatCategory>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SeatCategory> FindByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SeatCategory>> FindByPriceGreaterAsync(decimal price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SeatCategory>> FindByPriceLessAsync(decimal price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(SeatCategory seatCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(SeatCategory seatCategory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
