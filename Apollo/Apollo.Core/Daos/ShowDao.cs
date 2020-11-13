4dab210f395617cf00b39fdea41628a204a7893e
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 22:36:27 +0100

    Add ShowDao boilerplate

---
+++ Apollo/Apollo.Core/Daos/ShowDao.cs
@@ -0,0 +1,51 @@
+﻿using Apollo.Core.Interface.Daos;
+using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Daos
+{
+    public abstract class ShowDao : IShowDao
+    {
+        public Task<bool> DelegeAsync(Show show)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Show>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Show>> FindByCinemaHall(CinemaHall cinemaHall)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Show>> FindByDateAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Show>> FindByExactDateAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Show>> FindByMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(Show show)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Show show)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
