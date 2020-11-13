69d5208c7adc134ff6f3e4bfd9bfed4a43700428
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 22:32:23 +0100

    Add ReservationDao boilerplate

---
+++ Apollo/Apollo.Core/Daos/ReservationDao.cs
@@ -0,0 +1,41 @@
+﻿using Apollo.Core.Interface.Daos;
+using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Daos
+{
+    public abstract class ReservationDao : IReservationDao
+    {
+        public Task<bool> DeleteAsync(Reservation reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Reservation>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Reservation> FindByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Reservation>> FindByShow(Show show)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(Reservation reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Reservation reservation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
