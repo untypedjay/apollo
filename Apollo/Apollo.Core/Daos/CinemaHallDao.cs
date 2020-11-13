dee544973c8efad412ae41706889431d9943f3aa
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 22:26:50 +0100

    Add abstract class CinemaHallDao

---
+++ Apollo/Apollo.Core/Daos/CinemaHallDao.cs
@@ -0,0 +1,35 @@
+﻿using Apollo.Core.Interface.Daos;
+using Apollo.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Daos
+{
+    public abstract class CinemaHallDao : ICinemaHallDao
+    {
+        public Task<bool> DeleteAsync(CinemaHall cinemaHall)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CinemaHall>> FindAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CinemaHall> FindByNameAsync(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(CinemaHall cinemaHall)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(CinemaHall cinemaHall)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
