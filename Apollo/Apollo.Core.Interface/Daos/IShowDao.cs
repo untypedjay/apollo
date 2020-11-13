7eb8edece6a64e872acf078efbef44c05e0a5ec1
johannesdominik <huesers.johannes@gmail.com>
2020-11-13 21:45:21 +0100

    Add IShowDao interface

---
+++ Apollo/Apollo.Core.Interface/Daos/IShowDao.cs
@@ -0,0 +1,19 @@
+﻿using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Interface.Daos
+{
+    interface IShowDao
+    {
+        Task<bool> InsertAsync(Show show);
+        Task<IEnumerable<Show>> FindAllAsync();
+        Task<IEnumerable<Show>> FindByDateAsync(DateTime date);
+        Task<IEnumerable<Show>> FindByExactDateAsync(DateTime date);
+        Task<IEnumerable<Show>> FindByCinemaHall(CinemaHall cinemaHall);
+        Task<IEnumerable<Show>> FindByMovie(Movie movie);
+        Task<bool> UpdateAsync(Show show);
+        Task<bool> DelegeAsync(Show show);
+    }
+}
