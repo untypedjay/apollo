4efef2e8e959765074809c83ce82630eb18fc356
johannesdominik <huesers.johannes@gmail.com>
2020-11-14 15:41:48 +0100

    Add IConnectionFactory interface

---
+++ Apollo/Apollo.Core/ConfigurationUtil.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apollo.Core
+{
+    public static class ConfigurationUtil
+    {
+        private static IConfiguration configuration = null;
+
+        public static IConfiguration GetConfiguration() =>
+            configuration = configuration ?? new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+        public static (string connectionString, string providerName) GetConnectionParameters(string configName)
+        {
+            var connectionConfig = GetConfiguration().GetSection("ConnectionStrings").GetSection(configName);
+            return (connectionConfig["ConnectionString"], connectionConfig["ProviderName"]);
+        }
+    }
+}
---
+++ Apollo/Apollo.Core/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Apollo.Core
+{
+    public interface IConnectionFactory
+    {
+        string ConnectionString { get; }
+        string ProviderName { get; }
+        Task<DbConnection> CreateConnectionAsync();
+    }
+}
