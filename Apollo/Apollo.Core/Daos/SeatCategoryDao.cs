a36118b06ccf05c966b18a8333d226fe9bf9e28b
johannesdominik <huesers.johannes@gmail.com>
2020-11-15 10:49:44 +0100

    Implement SeatCategoryDao

--- Apollo/Apollo.Core/Daos/SeatCategoryDao.cs
+++ Apollo/Apollo.Core/Daos/SeatCategoryDao.cs
@@ -2,6 +2,7 @@
 using Apollo.Domain;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Apollo.Core.Daos
@@ -17,37 +18,66 @@ namespace Apollo.Core.Daos
 
         public virtual async Task<bool> DeleteAsync(SeatCategory seatCategory)
         {
-            throw new NotImplementedException();
+            return (await template.ExecuteAsync(
+                "DELETE FROM SeatCategory WHERE CategoryName=@cn",
+                new QueryParameter("@cn", seatCategory.Name)
+                )) == 1;
         }
 
         public virtual async Task<IEnumerable<SeatCategory>> FindAllAsync()
         {
-            throw new NotImplementedException();
+            return await template.QueryAsync<SeatCategory>("SELECT * FROM SeatCategory", MapRowToSeatCategory);
         }
 
         public virtual async Task<SeatCategory> FindByNameAsync(string name)
         {
-            throw new NotImplementedException();
+            return await template.QuerySingleAsync<SeatCategory>(
+                "SELECT * FROM SeatCategory WHERE CategoryName=@cn",
+                MapRowToSeatCategory,
+                new QueryParameter("@cn", name));
         }
 
         public virtual async Task<IEnumerable<SeatCategory>> FindByPriceGreaterAsync(decimal price)
         {
-            throw new NotImplementedException();
+            return await template.QueryAsync<SeatCategory>(
+                "SELECT * FROM SeatCategory WHERE Price>@price",
+                MapRowToSeatCategory,
+                new QueryParameter("@price", price));
         }
 
         public virtual async Task<IEnumerable<SeatCategory>> FindByPriceLessAsync(decimal price)
         {
-            throw new NotImplementedException();
+            return await template.QueryAsync<SeatCategory>(
+                "SELECT * FROM SeatCategory WHERE Price<@price",
+                MapRowToSeatCategory,
+                new QueryParameter("@price", price));
         }
 
         public virtual async Task<bool> InsertAsync(SeatCategory seatCategory)
         {
-            throw new NotImplementedException();
+            return (await template.ExecuteAsync(
+                "INSERT INTO SeatCategory (CategoryName, Price) VALUES (@cn, @price)",
+                new QueryParameter("@cn", seatCategory.Name),
+                new QueryParameter("@price", seatCategory.Price)
+                )) == 1;
         }
 
         public virtual async Task<bool> UpdateAsync(SeatCategory seatCategory)
         {
-            throw new NotImplementedException();
+            return (await template.ExecuteAsync(
+                "UPDATE SeatCategory SET Price=@price WHERE CategoryName=@cn",
+                new QueryParameter("@cn", seatCategory.Name),
+                new QueryParameter("@price", seatCategory.Price)
+                )) == 1;
+        }
+
+        private SeatCategory MapRowToSeatCategory(IDataRecord row)
+        {
+            return new SeatCategory
+            {
+                Name = (string)row["CategoryName"],
+                Price = (decimal)row["Price"]
+            };
         }
     }
 }
