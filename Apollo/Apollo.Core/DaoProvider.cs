656c80a2f0ddd2f256043402e8d39213c1ee792c
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-20 12:15:41 +0100

    Implement DaoProvider

---
+++ Apollo/Apollo.Core/DaoProvider.cs
@@ -0,0 +1,25 @@
+﻿using Apollo.Core.Interface.Daos;
+
+namespace Apollo.Core
+{
+    public class DaoProvider
+    {
+        public ICinemaHallDao CinemaHallDao { get; set; }
+        public IMovieDao MovieDao { get; set; }
+        public IReservationDao ReservationDao { get; set; }
+        public ISeatCategoryDao SeatCategoryDao { get; set; }
+        public ISeatDao SeatDao { get; set; }
+        public IShowDao ShowDao { get; set; }
+
+        public DaoProvider(ICinemaHallDao cinemaHallDao, IMovieDao movieDao, IReservationDao reservationDao,
+            ISeatCategoryDao seatCategoryDao, ISeatDao seatDao, IShowDao showDao)
+        {
+            CinemaHallDao = cinemaHallDao;
+            MovieDao = movieDao;
+            ReservationDao = reservationDao;
+            SeatCategoryDao = seatCategoryDao;
+            SeatDao = seatDao;
+            ShowDao = showDao;
+        }
+    }
+}
