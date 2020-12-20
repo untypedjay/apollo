1a373ab25c8715de0660dfa49658ca8f4d102e3f
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-20 12:17:59 +0100

    Add base service

---
+++ Apollo/Apollo.Core/Services/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace Apollo.Core.Services
+{
+    public abstract class Service
+    {
+        protected readonly DaoProvider DaoProvider;
+
+        protected Service(DaoProvider daoProvider)
+        {
+            DaoProvider = daoProvider;
+        }
+    }
+}
