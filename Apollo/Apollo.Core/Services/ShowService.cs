09583d52f862ddb9f12c0deba8b104f1f94284b4
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-20 12:39:29 +0100

    Implement ShowService

---
+++ Apollo/Apollo.Core/Services/ShowService.cs
@@ -0,0 +1,37 @@
+﻿using Apollo.Core.Interface.Services;
+using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Services
+{
+    public class ShowService : Service, IShowService
+    {
+        public ShowService(DaoProvider daoProvider) : base(daoProvider)
+        {
+        }
+
+        public async Task<IEnumerable<Show>> GetShowsByGenreSearch(string search)
+        {
+            var movies = await DaoProvider.MovieDao.FindByGenreAsync(search);
+            var shows = new List<Show>();
+            foreach (var movie in movies)
+            {
+                shows.AddRange(await DaoProvider.ShowDao.FindByMovieAsync(movie));
+            }
+            return shows;
+        }
+
+        public async Task<IEnumerable<Show>> GetShowsByMovieSearch(string search)
+        {
+            Movie movie = await DaoProvider.MovieDao.FindByTitleAsync(search);
+            return await DaoProvider.ShowDao.FindByMovieAsync(movie);
+        }
+
+        public async Task<IEnumerable<Show>> GetShowsToday()
+        {
+            return await DaoProvider.ShowDao.FindByExactDateAsync(DateTime.Now);
+        }
+    }
+}
