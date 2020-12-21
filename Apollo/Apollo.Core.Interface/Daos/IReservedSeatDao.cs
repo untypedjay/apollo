a7f56912a7f7b4a23516e5f5bd0cf58c0acbc46b
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-21 22:09:34 +0100

    Add IReservedSeatDao

---
+++ Apollo/Apollo.Core.Interface/Daos/IReservedSeatDao.cs
@@ -0,0 +1,12 @@
+﻿using Apollo.Domain;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Interface.Daos
+{
+    public interface IReservedSeatDao
+    {
+        Task<bool> InsertAsync(Reservation reservation, int seatNumber, int seatRow);
+        Task<bool> DeleteAsync(Reservation reservation, int seatNumber, int seatRow);
+        Task<bool> IsReservedAsync(Seat seat, Show show);
+    }
+}
