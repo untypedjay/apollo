40dd169f141ae40e1170658d7c819510cd3f071c
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-21 21:39:14 +0100

    Add reservation and seat service interfaces

---
+++ Apollo/Apollo.Core.Interface/Services/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿using Apollo.Domain;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Interface.Services
+{
+    public interface IReservationService
+    {
+        Task<long> CreateReservation(Show show, int seatNumber, int rowNumber);
+        Task<bool> DeleteReservation(long id);
+        Task<bool> GetReservationById(long id);
+    }
+}
---
+++ Apollo/Apollo.Core.Interface/Services/ISeatService.cs
@@ -0,0 +1,14 @@
+﻿using Apollo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Interface.Services
+{
+    public interface ISeatService
+    {
+        Task<IEnumerable<Seat>> GetSeatsByCinemaHall(CinemaHall cinemaHall);
+        Task<IEnumerable<Seat>> GetReservedSeatsByShow(Show show);
+    }
+}
