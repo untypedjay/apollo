ddd691791de08c1e85e2ba336a798d9889a50026
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-21 22:37:25 +0100

    Add MSSQLReservedSeatDao

---
+++ Apollo/Apollo.Core/Daos/MSSQLReservedSeatDao.cs
@@ -0,0 +1,7 @@
+﻿namespace Apollo.Core.Daos
+{
+    public class MSSQLReservedSeatDao : ReservedSeatDao
+    {
+        public MSSQLReservedSeatDao(IConnectionFactory connectionFactory) : base(connectionFactory) { }
+    }
+}
