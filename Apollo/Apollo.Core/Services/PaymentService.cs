ff5c38aa925de5af778814d3161d2ee7e915cc6b
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-12-22 19:03:29 +0100

    Implement payment service interface

---
+++ Apollo/Apollo.Core/Services/PaymentService.cs
@@ -0,0 +1,20 @@
+﻿using Apollo.Core.Interface.Services;
+using FHPay;
+using System;
+using System.Threading.Tasks;
+
+namespace Apollo.Core.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public async Task<PaymentResult> ProcessPayment(string cardNumber, decimal amount)
+        {
+            PaymentApi api = new PaymentApi(Environment.GetEnvironmentVariable("fhpay"));
+            CardValidationCode cardValidationCode = new CardValidationCode();
+            ExpirationDate expirationDate = new ExpirationDate();
+            CreditCardNumber creditCardNumber = new CreditCardNumber(cardNumber);
+            CreditCard creditCard = new CreditCard("Jeff Bezos", creditCardNumber, expirationDate, cardValidationCode);
+            return await api.CreateTransactionAsync(amount, creditCard, "This is a description");
+        }
+    }
+}
