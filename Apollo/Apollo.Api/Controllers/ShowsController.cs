ec1e9b2d1fd5c6f3bd7fd1459c1d781d572b4fd0
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-16 18:53:55 +0100

    Add ShowsController boilerplate

---
+++ Apollo/Apollo.Api/Controllers/ShowsController.cs
@@ -0,0 +1,24 @@
+﻿using Apollo.Core.Interface.Services;
+using Apollo.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Apollo.Api.Controllers
+{
+    public class ShowsController : Controller
+    {
+        public ShowsController(IShowService logic)
+        {
+            Logic = logic;
+        }
+
+        private IShowService Logic { get; }
+
+        [HttpGet]
+        public async Task<IEnumerable<Show>> GetAll()
+        {
+            return await Logic.GetShowsToday();
+        }
+    }
+}
