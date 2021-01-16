777a1e5a0cadf6dd8efa0d01a5187dd7cc060d65
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-16 18:46:54 +0100

    Configure startup

--- Apollo/Apollo.Api/Startup.cs
+++ Apollo/Apollo.Api/Startup.cs
@@ -1,14 +1,10 @@
+using Apollo.Core.Interface.Services;
+using Apollo.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Apollo.Api
 {
@@ -24,7 +20,16 @@ namespace Apollo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            })
+                .AddNewtonsoftJson()
+                .AddXmlDataContractSerializerFormatters();
+            services.AddScoped<IShowService, ShowService>();
+            // TODO: add other services here
+
+            services.AddOpenApiDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +48,10 @@ namespace Apollo.Api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3(settings => settings.Path = "/swagger");
+            app.UseReDoc(settings => settings.Path = "/redoc");
         }
     }
 }
