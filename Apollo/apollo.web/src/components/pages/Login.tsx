6c724f5b29c716476314f776abbaacae45a546a4
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-22 21:59:43 +0100

    Add structure of login form

--- Apollo/apollo.web/src/components/pages/Login.tsx
+++ Apollo/apollo.web/src/components/pages/Login.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [stayLoggedInValue, setStayLoggedInValue] = useState(false);
+
+  const handleInputChange = (event: FormEvent<HTMLInputElement>): void => {
+    const eventTarget: any = event.target;
+    if (eventTarget.name === 'email') {
+      setEmailValue(eventTarget.value);
+    } else if (eventTarget.name === 'password') {
+      setPasswordValue(eventTarget.value);
+    } else {
+      setStayLoggedInValue(eventTarget.checked);
+    }
+  };
+
+  const login = () => {
+
+  };
+
   return (
     <div className="login">
-
-      Login
+      <div className="login__form">
+        <input type="email" value={emailValue} onChange={handleInputChange} name="email"/>
+        <input type="password" value={passwordValue} onChange={handleInputChange} name="password"/>
+        <input type="checkbox" checked={stayLoggedInValue} onChange={handleInputChange} name="stayLoggedIn"/>
+        <button onClick={login}>Login</button>
+        <Link to={''}>Forgot password?</Link>
+      </div>
     </div>
   );
 }
