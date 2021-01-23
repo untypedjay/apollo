a6d884e38f89615620e894fec57a3eea41e29f4a
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-23 15:14:23 +0100

    Add Menu

---
+++ Apollo/apollo.web/src/components/organisms/Menu.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome, FaChair, FaFilm, FaCalendarAlt } from 'react-icons/fa';
+import MenuItem from '../molecules/MenuItem';
+import Logo from '../../images/apollo-logo.png';
+import './Menu.css';
+
+interface Props {
+  selected: string;
+}
+
+function Menu({ selected }: Props) {
+  const iconSize = '30px';
+
+  return (
+    <div className="menu">
+      <Link to="/"><img className="menu__logo" src={Logo} alt="Apollo Logo"/></Link>
+
+      <MenuItem
+        selected={selected === '/admin'}
+        route="/admin"
+        icon={<FaHome className="menu__icon" size={iconSize}/>}
+      />
+
+      <MenuItem
+        selected={selected === '/admin/shows'}
+        route="/admin/shows"
+        icon={<FaCalendarAlt className="menu__icon" size={iconSize}/>}
+      />
+
+      <MenuItem
+        selected={selected === '/admin/films'}
+        route="/admin/films"
+        icon={<FaFilm className="menu__icon" size={iconSize}/>}
+      />
+
+      <MenuItem
+        selected={selected === '/admin/halls'}
+        route="/admin/halls"
+        icon={<FaChair className="menu__icon" size={iconSize}/>}
+      />
+    </div>
+  );
+}
+
+export default Menu;
--- Apollo/apollo.web/src/components/pages/AdminArea.tsx
+++ Apollo/apollo.web/src/components/pages/AdminArea.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 
 function AdminArea() {
   return (
