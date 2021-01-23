dc01d043983c214355606570546a9958304c3f3d
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-23 20:12:01 +0100

    Add movieService

---
+++ Apollo/apollo.web/src/services/movieService.ts
@@ -0,0 +1,42 @@
+const BASE_ENDPOINT = 'http://localhost:5000/api/movies';
+
+interface Movie {
+  title: string;
+  description: string;
+  genre: string;
+  length: number;
+  actors: string;
+  imageURL: string;
+  trailerURL: string;
+}
+
+export async function fetchMovies() {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
+
+export async function updateMovie(movie: Movie) {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(movie)
+  });
+}
+
+export async function insertMovie(movie: Movie) {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(movie)
+  });
+}
+
+export async function deleteMovie(movie: Movie) {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(movie)
+  });
+}
