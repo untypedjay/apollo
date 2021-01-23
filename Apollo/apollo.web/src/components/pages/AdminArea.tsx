43d4bd2bd65123c25341d5c183bb49c464569bbf
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-23 15:41:53 +0100

    Add AdminArea

--- Apollo/apollo.web/src/components/pages/AdminArea.tsx
+++ Apollo/apollo.web/src/components/pages/AdminArea.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-
+import AdminContent from '../organisms/AdminContent';
+import Menu from '../organisms/Menu';
+import './AdminArea.css';
 
 function AdminArea() {
+  const location = useLocation();
+
   return (
     <div className="admin-area">
-      Admin Area
+      <Menu selected={location.pathname}/>
+      <AdminContent section={location.pathname}/>
     </div>
   );
 }
