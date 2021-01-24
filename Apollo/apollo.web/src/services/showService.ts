fb6ef43e513485bd780e56bd45b1f27a439c24a6
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2021-01-24 12:39:45 +0100

    Implement showService

--- Apollo/apollo.web/src/services/showService.ts
+++ Apollo/apollo.web/src/services/showService.ts
@@ -1,8 +1,40 @@
+import { Movie } from './movieService';
+
 const BASE_ENDPOINT = 'http://localhost:5000/api/shows';
+
+export type Show = {
+  timeStamp: string,
+  movie: Movie
+}
 
 export async function fetchShows() {
   return await fetch(BASE_ENDPOINT, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' }
+  });
+}
+
+export async function getShowCapacity(show: Show) {
+  const endpoint = `${BASE_ENDPOINT}/capacity`;
+  return await fetch(endpoint, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(show)
+  });
+}
+
+export async function insertShow(show: Show) {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(show)
+  });
+}
+
+export async function deleteShow(show: Show) {
+  return await fetch(BASE_ENDPOINT, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(show)
   });
 }
